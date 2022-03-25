b25d1723c4d64785174d8673ad58a9bc5f5c337e
sarahpicard <sarahannrey225@gmail.com>
2022-03-25 11:57:23 -0400

    stub EventDetails component

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -1,11 +1,14 @@
 import Events from "../../pages/Events/Events";
+import { Link } from "react-router-dom";
 
 const eventDetails = (props) => {
   return ( 
     <>
-      <h1>Event Details Here</h1>
+    <h2>{props.event.name}</h2>
+    <Link to="/eventdetail" state={props.event}>Event Detail</Link>
+    {/* <a href={props.event.url}>Ticket</a> */}
     </>
-   );
+  );
 }
  
 export default eventDetails;
