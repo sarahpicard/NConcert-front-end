c5a67a8735fc6dbcfce713a8e9d00881e918be72
hbarrons <hbarrons@gmail.com>
2022-03-25 11:32:03 -0400

    stub up event details component

---
+++ src/components/EventDetails/EventDetails.jsx
@@ -0,0 +1,11 @@
+import Events from "../../pages/Events/Events";
+
+const eventDetails = (props) => {
+  return ( 
+    <>
+      <h1>Event Details Here</h1>
+    </>
+   );
+}
+ 
+export default eventDetails;
