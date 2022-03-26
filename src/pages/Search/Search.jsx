9f6c5fd7fd3ffa482cfbfbf83c95e50166126064
sarahpicard <sarahannrey225@gmail.com>
2022-03-26 14:04:47 -0400

    add search functionality fo events page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,67 +2,43 @@ import * as eventService from '../../services/eventServices'
 import { useState } from 'react'
 
 const Search = (props) => {
-  // searching keyword only
-  const [searchKeywordData, setSearchKeywordData] = useState({
-    keyword: ''
-  })
-
-  // searching city only
-  const [searchCityData, setSearchCityData] = useState({
+  const [keywordData, setKeywordData] = useState({})
+  const [searchData, setSearchData] = useState({
+    keyword: '',
     city: ''
   })
-
-  // handle change of keyword input
-  const handleChangeKeyword = (evt) => {
-    setSearchKeywordData({...searchKeywordData, [evt.target.name]: evt.target.value})
+  // handle change of input
+  const handleChange = (evt) => {
+    setSearchData({...searchData, [evt.target.name]: evt.target.value})
   }
 
-  // handle change of city input
-  const handleChangeCity = (evt) => {
-    setSearchCityData({...searchCityData, [evt.target.name]: evt.target.value})
-  }
-
-  // handle keyword submit
-  const handleKeywordSubmit = async e => {
+  // handle submit
+  const handleSubmit = async e => {
     e.preventDefault()
     try {
-      // API call with search DATA (searchKeywordData)
+      eventService.getSearch(searchData.keyword, searchData.city)
+      .then(searchData => {
+        setKeywordData(searchData)
+      })
     } catch (err) {
       console.log(err)
     }
   }
 
-  // handle city submit
-  const handleCitySubmit = async e => {
-    e.preventDefault()
-    try {
-      // API call with search DATA (searchCityData)
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  const { keyword } = searchData
+  const { city } = searchData
 
-  // these seem to be connecting back to the actual paramater coming through the api (and assigning them state in the form of useState - above.)
-  const { keyword } = searchKeywordData
-  const { city } = searchCityData
-
-  // seems to be validating the form data using the const set above 
+  // form validation
   const isKeywordFormValid = () => {
     return !(keyword)
-  }
-  const isCityFormValid = () => {
-    return !(city)
   }
 
   return (
     <>
-      <form action="#" onSubmit={handleKeywordSubmit}>
-        <input type="text" value={keyword} name="keyword" placeholder="search event" onChange={handleChangeKeyword}/>
-        <button disabled={isKeywordFormValid()}>Search Keyword</button>
-      </form>
-      <form action="#" onSubmit={handleCitySubmit}>
-        <input type="text" value={city} name="city" placeholder="search city" onChange={handleChangeCity}/>
-        <button disabled={isCityFormValid()}>Search City</button>
+      <form action="#" onSubmit={handleSubmit}>
+        <input type="text" value={keyword} name="keyword" placeholder="search event" onChange={handleChange}/>
+        <input type="text" value={city} name="city" placeholder="search city" onChange={handleChange}/>
+        <button disabled={isKeywordFormValid()}>Search</button>
       </form>
     </>
   )
--- src/services/eventServices.js
+++ src/services/eventServices.js
@@ -1,28 +1,18 @@
 const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/events`
 
+// get all events
 async function getAllEvents() {
   const res = await fetch(BASE_URL)
   return await res.json()
 }
-// async function searchEvents() {
-//   const res = await fetch(BASE_URL).json?keyword={SOMETHING}&city={SOMETHING}&apikey=Kim5J88GrD7Fd32jnkeNCTtc4QARtrEv {
-//     method: 'POST'
-//   }
-// }
-// this is not right - we will need to refactor the correct url to make sure that it has accurate info and possibly assign variables for keyword and city 
 
-
-
-
-// async function searchEvents(){
-//   const res = await fetch(BASE_URL, {
-//     city: "input value here",
-//     keyword: "input value here"
-//   })
-//   return await res.json()
-// }
+// search events
+async function getSearch(keyword, city) {
+  const res = await fetch(`${BASE_URL}/search/${keyword}/${city}`)
+  return await res.json()
+}
 
 export {
   getAllEvents,
-  // searchEvents
+  getSearch, 
 }
