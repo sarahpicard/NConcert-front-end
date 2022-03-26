d9ec7c88ba413f3800571588efe2236493728e31
dcharr16 <davidcraigharrison@gmail.com>
2022-03-26 16:00:20 -0400

    add pagination button

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,19 +1,13 @@
-// import * as eventService from "./services/eventServices"
 import { useState, useEffect } from "react"
 import * as eventService from '../../services/eventServices.js'
 import EventDetails from "../../components/EventDetails/EventDetails.jsx"
 import Search from "../Search/Search.jsx"
 
-const Events = (props) => {
-
+const Events = (props) => {  
   return (
     <>
       <h1>Events Page</h1>
       <Search />
-      {props.events.map((event) =>
-        <EventDetails event={event} />
-      )}
-      <button>Next Page</button>
     </>
   )
 }
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,8 +1,10 @@
 import * as eventService from '../../services/eventServices'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import EventDetails from "../../components/EventDetails/EventDetails.jsx"
 
 const Search = (props) => {
   const [keywordData, setKeywordData] = useState({})
+  const[links, setLinks] = useState([])
   const [searchData, setSearchData] = useState({
     keyword: '',
     city: ''
@@ -25,6 +27,22 @@ const Search = (props) => {
     }
   }
 
+  const handleNextPage= async e => {
+    e.preventDefault()
+    try {
+      eventService.getSearch(searchData._links)
+      .then (searchData => {
+        setSearchData (searchData)
+      })
+    }catch (err) {
+    console.log(err)
+    }
+  }
+  useEffect(() => {
+    eventService.getSearch()
+    .then(allLinks => setLinks(allLinks))
+  }, [])
+
   const { keyword } = searchData
   const { city } = searchData
 
@@ -40,6 +58,10 @@ const Search = (props) => {
         <input type="text" value={city} name="city" placeholder="search city" onChange={handleChange}/>
         <button disabled={isKeywordFormValid()}>Search</button>
       </form>
+      {searchData._embedded.events.map((event) =>
+        <EventDetails event={event} />
+      )}
+      <button>Next Page</button>
     </>
   )
 }
