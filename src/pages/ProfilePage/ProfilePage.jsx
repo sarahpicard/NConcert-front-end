19bfc732e9d96c64e9f3ab76e9e440e27ab6d8bb
hbarrons <hbarrons@gmail.com>
2022-03-27 01:24:43 -0400

    add artist/genre/bio form to users  profile view

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -5,26 +5,60 @@ import { showProfile } from "../../services/profileService"
 const ProfilePage = (props) => {
   const [profile, setProfile] = useState()
   let location = useLocation()
+  const [profileData, setProfileData] = useState({
+    genre: '',
+    artist: '',
+    bio: '',
+  })
   
   useEffect(() => {
     showProfile(location.state.profile)
     .then(profileData => setProfile(profileData))
   })
 
+
+  const handleAddProfileData = (evt) => {
+    setProfileData({...profileData, [evt.target.name]: evt.target.value})
+  }
+
+  const submitProfileData = (evt) => {
+    evt.preventDefault()
+    try {
+      // profileService call here 
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const { bio } = profileData
+  const { genre } = profileData
+  const { artist } = profileData
+  
+
   return (
     <>
     {props.user.profile === location.state.profile._id  ? 
       <>
-        {console.log("userId: ", props.user._id)}
-        {console.log("profileId: ", location.state.profile._id)}
         <h1>My Profile</h1>
+        <h1>Edit Profile</h1>
+          <h4>Add your favorite artists and genres!</h4>
+          <form onSubmit={submitProfileData}>
+            <label>Bio:
+              <input type="text" value={bio} name="bio" autoComplete="off" onChange={handleAddProfileData}/>
+            </label>
+            <label>Genre:
+              <input type="text" value={genre} name="genre" autoComplete="off" onChange={handleAddProfileData}/>
+            </label>
+            <label>Artist:
+              <input type="text" value={artist} name="artist"  onChange={handleAddProfileData} />
+            </label>
+            <button type="submit">Add Artist</button>
+          </form>
       </>
       :
       <>
-        {console.log("userId: ", props.user.profile)}
+        {console.log("userId: ", props.user)}
         {console.log("profileId: ", location.state.profile._id)}
-        {/* {console.log(props.user)}
-        {console.log(props.profile)} */}
         <h1>Other Person's Profile</h1>
       </>
     }
