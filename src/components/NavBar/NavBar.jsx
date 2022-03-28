ad3ce568a81fd6e8bae4e9d471fad2fa0b96f347
sarahpicard <sarahannrey225@gmail.com>
2022-03-28 10:23:35 -0400

    add nav bar expansion and collapse

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,38 @@
 import { Link } from 'react-router-dom'
-import { ProSidebar, SidebarHeader, SidebarFooter, SidebarContent, MenuItem } from 'react-pro-sidebar'
+import { useState } from 'react'
+import { ProSidebar, SidebarHeader, SidebarFooter, SidebarContent, MenuItem, Menu } from 'react-pro-sidebar'
 
 import 'react-pro-sidebar/dist/css/styles.css'
 import './NavBar.css'
 
 const NavBar = ({ user, handleLogout }) => {
+  const [menuCollapse, setMenuCollapse] = useState(false)
+
+  const menuIconClick = () => {
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true)
+  }
   return (
     <>
+    <div id="header">
       {user ?
-      <ProSidebar className='custom-nav-bar'>
+      <ProSidebar collapsed={menuCollapse}>
         <SidebarHeader>
-          <Link to='/'>N'Concert</Link><br />
-          Welcome, {user.name}
+          <div className='logotext'>
+            <Link to='/'>N'Concert</Link><br />
+            Welcome, {user.name}
+          </div>
+          <div className="closemenu" onClick={menuIconClick}>
+            {menuCollapse ? (
+              <p>expand</p>
+            ) :
+              <p>collapse</p>
+            }
+          </div>
         </SidebarHeader>
         <SidebarContent>
           <ul>
-            <MenuItem>
+            <MenuItem active={true}>
               <Link to="/profiles">Users (friendsToggle)</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="" onClick={handleLogout}>LOG OUT</Link>
             </MenuItem>
             <MenuItem>
               <Link to="/changePassword">Change Password</Link>
@@ -35,11 +48,20 @@ const NavBar = ({ user, handleLogout }) => {
             </MenuItem>
           </ul>
         </SidebarContent>
+        <SidebarFooter>
+          <ul>
+            <MenuItem>
+              <Link to="" onClick={handleLogout}>LOG OUT</Link>
+            </MenuItem>
+          </ul>
+        </SidebarFooter>
       </ProSidebar>
       :
       <ProSidebar>
         <SidebarHeader>
-          <Link to='/'>N'Concert</Link>
+          <div className='logotext'>
+            <Link to='/'>N'Concert</Link>
+          </div>
         </SidebarHeader>
         <SidebarContent>
           <ul>
@@ -59,6 +81,7 @@ const NavBar = ({ user, handleLogout }) => {
         </SidebarContent>
       </ProSidebar>
       }
+    </div>
     </>
   )
 }
