9d021881365a98ee68a08169b098e52ddbac2843
sarahpicard <sarahannrey225@gmail.com>
2022-03-28 15:48:17 -0400

    stub basic information for event details page

--- src/pages/EventDetail/EventDetail.jsx
+++ src/pages/EventDetail/EventDetail.jsx
@@ -17,9 +17,37 @@ const EventDetail = () => {
 
   return(
     <>
-    <h1>Event</h1>
+    <img src={location.state.event.images.find(image => image.height > 110 && image.width > 100).url} alt="concertImage" />
     <h2>{location.state.event.name}</h2>
-      {/* <h1>{props.event.name}</h1>  */}
+    <p>Genre: {location.state.event.classifications[0].genre.name}</p>
+    <div>
+      <h6>
+        Venue: {location.state.event._embedded.venues[0].name} 
+        <span>- {location.state.event._embedded.venues[0].city.name}</span>
+        <span>, {location.state.event._embedded.venues[0].state.stateCode}</span>
+      </h6>
+      <p>Accessible seating: {location.state.event._embedded.venues[0].accessibleSeatingDetail}</p>
+    </div>
+    <div>
+      <h3>{location.state.event.dates.start.localTime}</h3>
+      <h3>{location.state.event.dates.start.localDate}</h3>
+    </div>
+    <button>
+      <a href={location.state.event.url}>Buy Tickets</a>
+    </button>
+    <button>
+      <a href={location.state.event.products[0].url}>Parking Info</a>
+    </button>
+    <h4>Public Presale: {location.state.event.sales.public.startDateTime}</h4>
+    <div>
+      <h6>Important Venue Information:</h6>
+      <p>{location.state.event.info}</p>
+      <p>Parking Information: {location.state.event._embedded.venues[0].parkingDetail}</p>
+    </div>
+    <div>
+      <img src={location.state.event.seatmap.staticUrl} alt="seatmap" style={{height: '300px' }} />
+    </div>
+
       {/* <h2>{props.events[0]._embedded.venues[0].name}</h2>
       <h2>{props.events[0].dates.start.localDate}</h2>
       <h2>{props.events[0].dates.start.localTime}</h2>
