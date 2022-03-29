8068aedb8751f8e14380f082a234bc983339eaf2
sarahpicard <sarahannrey225@gmail.com>
2022-03-29 19:59:38 -0400

    add hover effects to profile cards

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -25,18 +25,20 @@ const Profiles = () => {
             {profiles.map(profile =>
               <Col>
                 <div>
-                  <div className="card individual-cards" style={{ width: '18rem'}}>
-                    <Link to={`/profile/${profile._id}`} state={{profile}}>
-                      <img src="https://i.imgur.com/Wk1vQtF.png" alt="avatar" className='all-profiles-avatar-img'/>
-                    </Link>
-                      <div className="card-body">
-                        <h5 className="card-title" key={profile._id}>{profile.name}</h5>
-                        <p className="card-text">this will be where profile bio goes </p>
-                        <Link to={`/profile/${profile._id}`} state={{profile}}> 
-                          <i className='bx bx-chevrons-down bx-lg profile-more-info-btn'></i>
-                        </Link>
-                      </div>
-                  </div>
+                  <Link to={`/profile/${profile._id}`} state={{profile}}>
+                    <div className="card individual-cards" style={{ width: '18rem'}}>
+                      <Link to={`/profile/${profile._id}`} state={{profile}}>
+                        <img src="https://i.imgur.com/Wk1vQtF.png" alt="avatar" className='all-profiles-avatar-img'/>
+                      </Link>
+                        <div className="card-body">
+                          <h5 className="card-title" key={profile._id}>{profile.name}</h5>
+                          <p className="card-text">this will be where profile bio goes </p>
+                          <Link to={`/profile/${profile._id}`} state={{profile}}> 
+                            <i className='bx bx-chevrons-down bx-lg profile-more-info-btn'></i>
+                          </Link>
+                        </div>
+                    </div>
+                  </Link>
                 </div>
               </Col>
             )}
