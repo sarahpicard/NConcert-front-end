72f81c6d034263d78600a6e7d1727c0bb65e1bc5
hbarrons <hbarrons@gmail.com>
2022-03-29 13:42:26 -0400

    add search componenet to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,37 +1,28 @@
 import styles from './Landing.module.css'
 import { Link } from 'react-router-dom'
+import Search from "../Search/Search.jsx"
+import { useState, useEffect } from 'react'
+import * as profileService from '../../services/profileService'
 
 const Landing = ({ user }) => {
+  const [profiles, setProfiles] = useState([])
+
+  useEffect(()=> {
+    profileService.getAllProfiles()
+    .then(profiles => {setProfiles(profiles)})
+  }, [])
+
+
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
-      <h1>Welcome to N'Concert!</h1>
-      <div className='profile-details'>
-        <Link to='/profile/edit'>
-          Edit Profile
-        </Link>
-        <br /><br />
-        <div>Bio:</div> 
-        <br />
-        <div>
-          My Favorite Genres:
-          <br />
-          My Favorit Artists:
-        </div>
-        <br />
-        <div>
-          My Friends:
-        </div>
-      </div>
-      <br />
-      <h1>My events</h1>
-      <div>
-        Interested:
-      </div>
-      <div>
-        Attending:
-      </div>
-    </main>
+    <>
+      <h1>N'Concert</h1>
+      {console.log(profiles)}
+      {profiles.map(profile => {
+        console.log(profile)
+      })}
+
+      <Search />
+    </>
   )
 }
 
