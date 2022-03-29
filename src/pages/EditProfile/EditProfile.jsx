f9be5f98d8557d90fbd1e23b406e75f558e5edd3
hbarrons <hbarrons@gmail.com>
2022-03-29 16:13:26 -0400

    stub up edit/update profile page and functions

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -15,41 +15,59 @@ const EditProfile = (props) => {
   useEffect(() => {
     profileService.showProfile(props.user.profile)
     .then(data => setProfile(data))
-  }, [])
-
-  const handleUpdateProfile = (evt) => {
-    console.log("something here")
-  }
+  }, [profileData])
 
   const handleUpdateProfileData = (evt) => {
     setProfileData({...profileData, [evt.target.name]: evt.target.value})
   }
 
-  
+  const handleUpdateBio = (evt) => {
+    console.log("update bio")
+  }
 
-  const { bio } = profileData
+  const handleAddGenre = (evt) => {
+    console.log("add genre")
+  }
+
+  const handleAddArtist = (evt) => {
+    console.log("add artist")
+  }
+
+  const handleUpdateSpotify = (evt) => {
+    console.log("update spotify")
+  }
+
+    const { bio } = profileData
   const { genre } = profileData
   const { artist } = profileData
   const { spotify } = profileData
 
   return(
     <>
-      <h1>Edit Profile Page</h1>
-      <h4>Add or Delete your favorite artists and genres:</h4>
-      <form onSubmit={handleUpdateProfile}>
+      <h4>Make Profile Updates:</h4>
+      <form onSubmit={handleUpdateBio}>
         <label>Bio:
           <input type="text" value={bio} name="bio" autoComplete="off" onChange={handleUpdateProfileData}/>
         </label>
-        <label>Genre:
+        <button type="submit">Update Profile</button>
+      </form>
+      <form onSubmit={handleAddGenre}>
+        <label>Add a Genre:
           <input type="text" value={genre} name="genre" autoComplete="off" onChange={handleUpdateProfileData}/>
         </label>
-        <label>Artist:
-          <input type="text" value={artist} name="artist"  onChange={handleUpdateProfileData} />
+        <button type="submit">Add Genre</button>
+      </form>
+      <form onSubmit={handleAddArtist}>
+        <label>Add an Artist:
+          <input type="text" value={artist} name="artist" autoComplete="off" onChange={handleUpdateProfileData}/>
         </label>
-        <label>Spotify Playlist:
-          <input type="text" value={spotify} name="spotify"  onChange={handleUpdateProfileData} />
+        <button type="submit">Add Artist</button>
+      </form>
+      <form onSubmit={handleAddArtist}>
+        <label>New Spotify Playlist:
+          <input type="text" value={artist} name="artist" autoComplete="off" onChange={handleUpdateProfileData}/>
         </label>
-        <button type="submit">Update Profile</button>
+        <button type="submit">Update Playlist</button>
       </form>
       <div>
       <>
