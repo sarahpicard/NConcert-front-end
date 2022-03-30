592ce90c6800f71a556ff743f0c8d3120307c802
hbarrons <hbarrons@gmail.com>
2022-03-30 12:18:15 -0400

    add my event details from api to state

--- src/pages/MyEventDetails/MyEventDetails.jsx
+++ src/pages/MyEventDetails/MyEventDetails.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react"
 import * as eventService from '../../services/eventServices'
+import { useLocation } from "react-router-dom"
 
 
 const MyEventDetails = (props) => {
   const [event,setEvent] = useState([])
+  let location = useLocation()
 
 
   useEffect(() => {
-    eventService.getEvent(props.event.eventId)
+    console.log(location.state.eventId)
+    eventService.getEvent(location.state.eventId)
     .then(eventData => setEvent(eventData))
-  }, [props.event.eventId])
+  }, [location.state.eventId])
 
   return ( 
     <>
