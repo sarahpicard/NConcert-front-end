542127b0777bb02fcee95e0593d76d9311395140
sarahpicard <sarahannrey225@gmail.com>
2022-03-29 21:11:43 -0400

    add conditional greeting rendering to the landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -15,12 +15,21 @@ const Landing = ({ user }) => {
 
   return (
     <>
-      <h1>N'Concert</h1>
-      {console.log({user})}
+    {user ?
+      <div>
+        <h1>Hello, {user.name.charAt(0).toUpperCase() + user.name.slice(1)}! 👋</h1>
+        <h5>Welcome back!</h5>
+        <Link to={`/profile/${user?.profile}`} state={user}>
+          View My Profile
+        </Link>
+      </div>
+      : 
+      <div>
+        <h1>Hello, Stranger 👋</h1>
+        <h5>Welcome to the party!</h5>
+      </div>
+    }
 
-      <Link to={`/profile/${user?.profile}`} state={user}>
-        My Profile
-      </Link>
       <h1>Search Events!</h1>
       <Search />
     </>
